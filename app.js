e296a9d8a246285bc476c43558d72fab2f5458a9
jwin4740 <jwin4740@gmail.com>
2018-01-13 12:13:50 -0500

    iteration working

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var options = { format: 'Letter', orientation: 'landscape' };
 
 let arr = [1,4,5,6];
 
-const template = `
+const template = cards => `
 <html>
 <style>
     #main {
@@ -37,46 +37,36 @@ const template = `
 
 <body>
     <div id="main">
-        <div class="card posy">
-            <div class="card-content">
-                <h3 class="card-title">General</h3>
-                <hr>
-                <ol id="bullets">
-                    <li>hello</li>
-                    <li>hello</li>
-                    <li>hello</li>
-                    <li>hello</li>
-                    <li>hello</li>
-                </ol>
-            </div>
-        </div>
-
-                
-           
-        <div class="card posy">
-            <div class="card-content">
-                <h3 class="card-title">General</h3>
-                <hr>
-                <ol id="bullets">
-                    <li>hello</li>
-                    <li>hello</li>
-                    <li>hello</li>
-                    <li>hello</li>
-                    <li>hello</li>
-                </ol>
-            </div>
-        </div>
+    ${cards}
    
     </div>
 </body>
 
-</html>
+</html>`;
 
-
+const createCard = () => `
+<div class="card posy">
+<div class="card-content">
+    <h3 class="card-title">General</h3>
+    <hr>
+    <ol id="bullets">
+        <li>hello</li>
+        <li>hello</li>
+        <li>hello</li>
+        <li>hello</li>
+        <li>hello</li>
+    </ol>
+</div>
+</div>
 `;
-console.log(template);
 
-pdf.create(template, options).toFile('./businesscard.pdf', function(err, res) {
+const cardArray = [];
+for(let i = 0; i < 10; i++) {
+    cardArray.push(createCard());
+}
+const html = template(cardArray.join(''));
+
+pdf.create(html, options).toFile('./businesscard.pdf', function(err, res) {
     if (err) return console.log(err);
     console.log(res); // { filename: '/app/businesscard.pdf' }
   });
